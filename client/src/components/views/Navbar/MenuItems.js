5ffbce5fc7e71fa021218e3dde5854d886c20e4e
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-11 01:44:39 +0900

    navbar menu setting finish

---
+++ client/src/components/views/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: '뉴미디어소프트웨어',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: '뉴미디어웹솔루션',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: '뉴미디어디자인',
+        url: '#',
+        cName: 'nav-links',
+    },
+]
