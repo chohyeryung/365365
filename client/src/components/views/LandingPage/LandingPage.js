fdb9e51bde0f5a7b4d9a689452696b06742dfe20
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-11 01:45:51 +0900

    table box default setting

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landingpage.scss';
+
+function LandingPage() {
+    return (
+        <div className="content-table">
+            white table
+        </div>
+    )
+}
+
+export default LandingPage
