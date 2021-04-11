26886826533854b521a117c681bb149b94290f3f
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-04-12 00:36:38 +0900

    db settings

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 1000
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('hello word'))
+
+app.post('/student', (req, res) => {
+    let student = req.body;
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
