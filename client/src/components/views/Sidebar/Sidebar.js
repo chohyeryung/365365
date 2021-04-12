0386ac2e4fb0404398400703dea00a500d2be916
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-12 23:26:47 +0900

    sidebar button handler finish

--- client/src/components/views/Sidebar/Sidebar.js
+++ client/src/components/views/Sidebar/Sidebar.js
@@ -1,28 +1,41 @@
 import React, { useState } from 'react'
-import { MenuItems } from './MenuItems';
 import './sidebar.scss'
 
 function Sidebar() {
 
-    const [clicked, setClicked] = useState(false)
+    const [first, setFirst] = useState(true)
+    const [second, setSecond] = useState(false)
+    const [third, setThird] = useState(false)
 
-    const onHandleClickMenu = () => {
-        setClicked(!clicked)
+    const handleFirst = () => {
+        setFirst(!first);
+    }
+
+    const handleSecond = () => {
+        setSecond(!second);
+    }
+
+    const handleThird = () => {
+        setThird(!third);
     }
 
     return (
         <ul className="side-menu-container">
-            {MenuItems.map((item, index) => {
-                return(
-                    <li className={clicked ? 'side-menu active' : 'side-menu'} key={index} onClick={onHandleClickMenu}>
-                        <div>
-                            <span className={item.cName} href={item.url}>
-                                {item.title}
-                            </span>
-                        </div>
-                    </li>
-                )
-            })}
+            <li className={first ? 'side-menu clicked' : 'side-menu'} onClick={handleFirst}>
+                <div>
+                    <span>1학년</span>
+                </div>
+            </li>
+            <li className={second ? 'side-menu clicked' : 'side-menu'} onClick={handleSecond}>
+                <div>
+                    <span>2학년</span>
+                </div>
+            </li>
+            <li className={third ? 'side-menu clicked' : 'side-menu'} onClick={handleThird}>
+                <div>
+                    <span>3학년</span>
+                </div>
+            </li>
         </ul>
     )
 }
