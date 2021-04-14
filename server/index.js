760f7f383dabcd7c8ca2e8ead83e2640c9f097aa
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-04-15 01:26:54 +0900

    connect table check_students

--- server/index.js
+++ server/index.js
@@ -10,11 +10,15 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     db.query("SELECT * FROM check_students", function(error, results){
         console.log(results);
+        //res.send({ students:results })
     });
 });
 
-app.post('/student', (req, res) => {
+app.post('/check', (req, res) => {
     let student = req.body;
+    let temperature = student.temp; //학생이 직접 입력한 온도 가져옴(form)
+    // let scode = student.code; ?? -> 학생증에 찍힌 코드를 가져옴
+    //scode where like로 찾아서 insert
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
