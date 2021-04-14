a5e95df81bdd4b3e20161c3d8584350e0f33a955
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-04-15 01:15:29 +0900

    connect table check_students

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-const port = 1000
+const app = express();
+const port = 1000;
 const db = require('./db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    db.query("SELECT * FROM test", function(error, results){
+    db.query("SELECT * FROM check_students", function(error, results){
         console.log(results);
     });
-})
+});
 
 app.post('/student', (req, res) => {
     let student = req.body;
-})
+});
 
-app.listen(port, () => console.log(`app listening on port ${port}`))
+app.listen(port, () => console.log(`app listening on port ${port}`));
