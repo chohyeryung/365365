97aaf845955347062f1d80a6242857ee2a272f97
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-15 01:40:44 +0900

    menu design change, put example data in table

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,21 @@ import './landingpage.scss';
 function LandingPage() {
     return (
         <div className="content-table">
-            white table
+          <table>
+              <tr className="table-head">
+                <td style={{ width: '100px' }}>학번</td>
+                <td style={{ width: '100px' }}>이름</td>
+                <td style={{ width: '100px' }}>시간</td>
+                <td style={{ width: '100px' }}>체온</td>
+              </tr>
+              {/* map 함수를 이용해서 서버에서 json 형태로 받아오면 데이터 추출해서 row 추가하기 */}
+              <tr className="first">
+                <td>3310</td>
+                <td>강채현</td>
+                <td>08:24</td>
+                <td>36.3</td>
+              </tr>
+          </table>
         </div>
     )
 }
