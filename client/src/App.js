a45409390c5a5eac11ac38a18a2b0278dbeabe61
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-04-16 21:41:47 +0900

    move landing page components to teacher page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import './App.scss';
-import { Suspense } from 'react';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import Navbar from './components/views/Navbar/Navbar';
-import Sidebar from './components/views/Sidebar/Sidebar';
 
 function App() {
   return (
-    <Suspense fallback = {( <div>Loading...</div> )}>
-      <div style={{ position: 'absolute', top:0, left: 0, width: '100%', height: '100%', backgroundColor: '#008156' }}>
-        <Navbar />
-        <div style={{ display: 'flex', height: '100%' }}>
-          <Sidebar />
-          <LandingPage />
-        </div>
-      </div>
-      
-    </Suspense>
+    <div>
+
+    </div>
   );
 }
 
