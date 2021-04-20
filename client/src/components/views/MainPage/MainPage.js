caa3c4589a66faa65daa94c61e318817b3c67cb6
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-04-21 00:51:22 +0900

    first page style

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./mainpage.scss";
 
 function MainPage() {
   return (
-    <div>
+    <div className="main_container">
+      <p className="top_text">더욱 간편한 일일 자가진단 서비스</p>
+      <p className="bottom_text">365, 36.5</p>
+
       <Link to={{ pathname: "/student" }}>
-        <div>
-          <h2>365, 36.5</h2>
-          <h5>학생증 리더기 접속</h5>
+        <div className="outline_circle">
+          <div className="inline_circle">
+            <div className="go_student">
+              <p className="student_top">365, 36.5</p>
+              <p className="student_bottom">학생증 리더기 접속</p>
+            </div>
+          </div>
         </div>
       </Link>
+
       <Link to={{ pathname: "/login" }}>
-        <h3>관리자로 로그인</h3>
+        <p className="go_login">관리자로 로그인</p>
       </Link>
     </div>
   );
