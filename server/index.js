ce60019cf4d0d30b791b542caca50d0367652ba0
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-04-21 23:28:31 +0900

    inserting student

--- server/index.js
+++ server/index.js
@@ -7,10 +7,29 @@ const db = require('./db');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    db.query("SELECT * FROM check_students", function(error, results){
-        console.log(results);
-        //res.send({ students:results })
+app.get('/inserting', (req, res) => {
+
+    // const info = req.body;
+    // let scode = info.code;  //학생 고유 코드
+    //let tmp = info.temperture;   //학생 온도
+    let scode = "BC2110101";
+
+    const diff_sql = "SELECT * FROM students WHERE banum = ?";
+    const insert_sql = "INSERT INTO check_student (stnum, name, temp, date, checked) VALUES (?, ?, ?, ?, ?)";
+
+    db.query(diff_sql, [scode], function(error, student){
+        if(student.length == 0) {
+            alert("해당 학생은 존재하지 않습니다.")
+        }
+        let hakbun = student[0].stnum;
+        // let bacode = student[0].banum;
+        let name = student[0].name;
+        let now = new Date();
+        
+        db.query(insert_sql, [hakbun, name, tmp, now, 1], function(error2, result) {
+            if(erorr2) throw error2;
+            alert('좋은 하루 되세요');
+        })
     });
 });
 
