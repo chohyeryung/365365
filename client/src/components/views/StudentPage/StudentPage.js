a69e319ce471382b8a8771bd3ac56b7cfd0ff450
jimin-oh <wlals6828@daum.net>
2021-04-22 19:44:45 +0900

    BarcodeScanner

--- client/src/components/views/StudentPage/StudentPage.js
+++ client/src/components/views/StudentPage/StudentPage.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
-function StudentPage() {
+
+const StudentPage = () => {
+
+  const handleScan = (result)=>{
+    
+  }
+
+  const handleError = (err)=>{
+    console.error(err)
+  }
+
+
+  const [ data, setData ] = React.useState('Not Found');
+ 
   return (
     <div>
-      <Link to={{ pathname: "/teacher" }}>학생 리스트 보기</Link>
-      <h1>StudentPage</h1>
+      <div className="barcodwebcam">
+        <BarcodeScannerComponent
+          width={800}
+          height={600}
+          onUpdate={(err, result) => {
+            if (result) handleScan(result)
+            else handleError(err)
+          }}
+        />
+      </div>
+      
     </div>
-  );
+  )
+
 }
 
 export default StudentPage;
