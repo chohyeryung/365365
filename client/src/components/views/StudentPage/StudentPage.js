a478a5694eea7d7f3f19adb16cf2bd41a43a3221
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-05-28 14:06:33 +0900

    student page

--- client/src/components/views/StudentPage/StudentPage.js
+++ client/src/components/views/StudentPage/StudentPage.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { Link, useHistory } from "react-router-dom";
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
+import "./studentpage.scss";
+import back from './icon/back_icon.png'
 
-function StudentPage() {
+const StudentPage = () => {
+
+  const history = useHistory();
+  const handleScan = (result)=>{
+
+    // 코드 확인
+
+    
+    history.push({
+      pathname: "/InputTemp",
+      state: {student: "3410오지민"}
+    })
+
+  }
+
+  const handleError = (err)=>{
+    console.log(err)
+  }
+
+ 
   return (
-    <div>
-      <Link to={{ pathname: "/teacher" }}>학생 리스트 보기</Link>
-      <h1>StudentPage</h1>
-    </div>
-  );
+      <div className="main_container">
+        <img src={back} className="back_icon" onClick={()=>{ history.goBack();}}/>
+        <div className="text_top">학생증 바코드를 찍어주세요</div>
+        <div className="barcode_scanner">
+          <BarcodeScannerComponent
+            onUpdate={(err, result) => {
+              if (result) handleScan(result)
+              else handleError(err)
+            }}
+          />
+        </div>
+      </div>
+      
+  )
+
 }
 
 export default StudentPage;
